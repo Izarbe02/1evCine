9e52e44a968afc2b7d36721b9126576be1bc5738
Guillermo <licencias@svalero.com>
2024-11-13 16:49:32 +0100

    fetchMain

---
+++ Frontend/JavaScript/main.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const HomePeliculas = document.querySelector(".cartelera__images");
+
+    fetch("https://localhost:7141/api/Pelicula")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error al obtener las películas");
+            }
+            return response.json();
+        })
+        .then(peliculas => {
+            peliculas.slice(0, 4).forEach(Pelicula => {
+                const movieItem = document.createElement("div");
+                movieItem.classList.add("pelicula"); 
+                movieItem.innerHTML = `
+                <img src="${Pelicula.imagen}" alt="${Pelicula.nombre}" class="movie-grid__image">
+                `;
+                HomePeliculas.appendChild(movieItem);
+
+            });
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            peliculasContainer.innerHTML = "<p>Error al cargar las películas</p>";
+        });
+});
