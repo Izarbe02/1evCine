62cc2737b06e36dc7e1c52f0c676bc80b891e4b1
guillermoCarrion <a27306@svalero.com>
2024-11-17 20:30:21 +0100

    creacion paginas asiento

---
+++ Frontend/JavaScript/asientos.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const asientosContainer = document.querySelector(".asientos-container");
+
+    fetch("https://localhost:7141/api/Pelicula")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error al obtener las películas");
+            }
+            return response.json();
+        })
+        .then(peliculas => {
+            peliculas.forEach(Pelicula => {
+                const movieItem = document.createElement("div");
+                movieItem.classList.add("movie-grid__item"); 
+                movieItem.innerHTML = `
+                <img src="${Pelicula.imagen}" alt="${Pelicula.nombre}" class="movie-grid__image">
+                <a href="#" class="movie-grid__button">SABER MÁS</a>
+                `;
+
+                const saberMasButton = movieItem.querySelector('.movie-grid__button');
+
+                saberMasButton.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    window.location.href = `pelicula.html?id=${Pelicula.id}`
+                })
+
+                peliculasContainer.appendChild(movieItem);
+
+            });
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            peliculasContainer.innerHTML = "<p>Error al cargar las películas</p>";
+        });
+});
+
