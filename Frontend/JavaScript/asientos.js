a4c03f305011c8640b44dedebc7b39e0222528cc
guillermoCarrion <a27306@svalero.com>
2024-11-20 12:26:06 +0100

    añadida vision asientos seleccionados

--- Frontend/JavaScript/asientos.js
+++ Frontend/JavaScript/asientos.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const asientosContainer = document.querySelector(".asientos-container");
     const botonReserva = document.querySelector(".reservar-button");
+    const divAsientosSeleccionados = document.querySelector(".asientos-seleccionados");
+    const divPrecio = document.querySelector(".precio-total");
+    let precioTotal = 0;
     let asientosSeleccionados = [];
 
     const params = new URLSearchParams(window.location.search);
@@ -20,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 asientoItem.classList.add("asiento");
                 asientoItem.classList.add(Asiento.estaReservado ? "ocupado" : "disponible");
                 asientoItem.dataset.idAsiento = Asiento.idAsiento;
+                asientoItem.dataset.precio = Asiento.precio;
 
                 asientoItem.innerHTML = `
               <article class="asiento">
@@ -28,17 +32,20 @@ document.addEventListener("DOMContentLoaded", function () {
                  </div> 
               </article>`;
 
-
-
                 // Agregar evento de clic para seleccionar asientos
                 if (!Asiento.estaReservado) {
                     asientoItem.addEventListener("click", function () {
+                        const precio = parseFloat(asientoItem.dataset.precio);
                         if (asientoItem.classList.contains("seleccionado")) {
                             asientoItem.classList.remove("seleccionado");
                             asientosSeleccionados = asientosSeleccionados.filter(id => id !== Asiento.idAsiento);
+                            precioTotal -= precio;
+                            actualizarAsientosSeleccionados();
                         } else {
                             asientoItem.classList.add("seleccionado");
                             asientosSeleccionados.push(Asiento.idAsiento);
+                            precioTotal +=precio;
+                            actualizarAsientosSeleccionados();
                         }
                         console.log("Asientos seleccionados:", asientosSeleccionados);
                     });
@@ -53,13 +60,25 @@ document.addEventListener("DOMContentLoaded", function () {
             asientosContainer.innerHTML = "<p>Error al cargar los asientos</p>";
         });
 
-
+    function actualizarAsientosSeleccionados() {
+        if(asientosSeleccionados.length === 0){
+            divAsientosSeleccionados.style.display = "none";
+            divPrecio.style.display = "none";
+        }else{
+            divAsientosSeleccionados.style.display = "block";
+            divPrecio.style.display = "block";
+            divAsientosSeleccionados.textContent = `Asientos seleccionados: ${asientosSeleccionados.join(", ")}`;
+            divPrecio.textContent = `Precio total: ${precioTotal.toFixed(2)}€`
+        }
+        }
 
     botonReserva.addEventListener("click", function () {
         if (asientosSeleccionados.length === 0) {
             alert("No has seleccionado ningún asiento.");
             return;
         }
+
+        /*
         console.log("ID de la sesión:", idUrl);
         if (!idUrl) {
             console.error("El ID de la sesión no está definido.");
@@ -69,6 +88,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(asientosSeleccionados.every(id => typeof id === "number"));
 
         console.log("Cuerpo de la solicitud:", JSON.stringify({ asientosIds: asientosSeleccionados }));
+        */
 
         fetch(`https://localhost:7141/api/Sesion/${idUrl}/asientos/reservar`, {
             method: "POST",
